4d76a40425079a2fad2a110c390c425aa5de514c
ChristopherDiehl <csd92@scarletmail.rutgers.edu>
2016-04-04 18:02:32 -0400

    testing json writer for mem leaks

--- Indexer/jsonWriter.c
+++ Indexer/jsonWriter.c
@@ -1,5 +1,6 @@
 #include "jsonWriter.h"
-
+#include "frequencyList.h"
+#include "fileTokenizer.h"
 /* Writes a frequency list to a file given into it by filename
  *BY CALLING jsonWRITE YOU DELETE THE FREQUENCY LIST
  *DO NOT CALL THEN EXPECT TO use Frequency List later
@@ -132,4 +133,36 @@ char * getJsonRecord (char * token, char * frequency, int trailing){
 		free(frequency);
     return entry;
 
+}
+
+int main (int argc, char **argv){
+  
+  if(argc !=3)
+  {
+    printf("[-]Please enter valid args\n" );
+    return -1 ; 
+  } 
+
+  TokenizerT * tk = Tokenize(argv[2]);
+  FrequencyList * fList = getFrequencyList();
+  while(1)
+  {
+    char * res = GetToken(tk);
+    if(res != 0 && strcmp(res, "") != 0){
+      //printf("Trying to add \"%s\" from %s to list\n", res, argv[1]);
+      addToList(argv[2], res, fList);
+
+      
+    }
+    if(res == 0)
+      break ; 
+    //printf("%s\n",res);
+
+  } 
+  jsonWrite(fList,argv[1]);
+
+  deleteList(fList);
+  tkDestroy(tk);
+
+  return 0;
 }
