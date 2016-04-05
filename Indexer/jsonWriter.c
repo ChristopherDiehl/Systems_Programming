a64fc65d900236be6098603f025add647f597593
ChristopherDiehl <csd92@scarletmail.rutgers.edu>
2016-04-05 11:55:24 -0400

    hopefully found all mem leaks.. used sizeof(str) instead of strlen(str)+1... Rookie mistake

--- Indexer/jsonWriter.c
+++ Indexer/jsonWriter.c
@@ -1,6 +1,5 @@
 #include "jsonWriter.h"
-#include "frequencyList.h"
-#include "fileTokenizer.h"
+
 /* Writes a frequency list tov a file given into it by filename
  *BY CALLING jsonWRITE YOU DELETE THE FREQUENCY LIST
  *DO NOT CALL THEN EXPECT TO use Frequency List later
@@ -134,34 +133,4 @@ char * getJsonRecord (char * token, char * frequency, int trailing){
 
 }
 
-int main (int argc, char **argv){
 
-  if(argc !=3)
-  {
-    printf("[-]Please enter valid args\n" );
-    return -1 ; 
-  } 
-
-  TokenizerT * tk = Tokenize(argv[2]);
-  FrequencyList * fList = getFrequencyList();
-  while(1)
-  {
-    char * res = GetToken(tk);
-    if(res != 0 && strcmp(res, "") != 0){
-      //printf("Trying to add \"%s\" from %s to list\n", res, argv[1]);
-      addToList(argv[2], res, fList);
-
-      
-    }
-    if(res == 0)
-      break ; 
-    //printf("%s\n",res);
-
-  } 
-  jsonWrite(fList,argv[1]);
-
-  deleteList(fList);
-  tkDestroy(tk);
-
-  return 0;
-}
